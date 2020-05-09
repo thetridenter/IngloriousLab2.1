55885c71eba94e8714e7008be5ee90a650f52936
thetridenter <kostia.perevertailo@icloud.com>
2020-05-09 17:13:49 +0300

    uploaded

---
+++ IngloriousLab2/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace IngloriousLab2
+{
+    class Program
+    {
+        static void Main()
+        {
+            double num;
+
+            Console.WriteLine("Введіть номер квартири (1-45)");
+            num = double.Parse(Console.ReadLine());
+
+            if (num > 45)
+                Console.WriteLine("Введено некоректний номер квартири");
+            if (num < 0)
+                Console.WriteLine("Введено некоректний номер квартири");
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Квартира розташована на 1-ому поверсі");
+                    break;
+                case 2:
+                    Console.WriteLine("Квартира розташована на 1-ому поверсі");
+                    break;
+                case 3:
+                    Console.WriteLine("Квартира розташована на 1-ому поверсі");
+                    break;
+                case 4:
+                    Console.WriteLine("Квартира розташована на 1-ому поверсі");
+                    break;
+                case 5:
+                    Console.WriteLine("Квартира розташована на 1-ому поверсі");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Квартира розташована на 2-ому поверсі");
+                    break;
+                case 7:
+                    Console.WriteLine("Квартира розташована на 2-ому поверсі");
+                    break;
+                case 8:
+                    Console.WriteLine("Квартира розташована на 2-ому поверсі");
+                    break;
+                case 9:
+                    Console.WriteLine("Квартира розташована на 2-ому поверсі");
+                    break;
+                case 10:
+                    Console.WriteLine("Квартира розташована на 2-ому поверсі");
+                    break;
+
+                case 11:
+                    Console.WriteLine("Квартира розташована на 3-ому поверсі");
+                    break;
+                case 12:
+                    Console.WriteLine("Квартира розташована на 3-ому поверсі");
+                    break;
+                case 13:
+                    Console.WriteLine("Квартира розташована на 3-ому поверсі");
+                    break;
+                case 14:
+                    Console.WriteLine("Квартира розташована на 3-ому поверсі");
+                    break;
+                case 15:
+                    Console.WriteLine("Квартира розташована на 3-ому поверсі");
+                    break;
+
+                case 16:
+                    Console.WriteLine("Квартира розташована на 4-ому поверсі");
+                    break;
+                case 17:
+                    Console.WriteLine("Квартира розташована на 4-ому поверсі");
+                    break;
+                case 18:
+                    Console.WriteLine("Квартира розташована на 4-ому поверсі");
+                    break;
+                case 19:
+                    Console.WriteLine("Квартира розташована на 4-ому поверсі");
+                    break;
+                case 20:
+                    Console.WriteLine("Квартира розташована на 4-ому поверсі");
+                    break;
+
+                case 21:
+                    Console.WriteLine("Квартира розташована на 5-ому поверсі");
+                    break;
+                case 22:
+                    Console.WriteLine("Квартира розташована на 5-ому поверсі");
+                    break;
+                case 23:
+                    Console.WriteLine("Квартира розташована на 5-ому поверсі");
+                    break;
+                case 24:
+                    Console.WriteLine("Квартира розташована на 5-ому поверсі");
+                    break;
+                case 25:
+                    Console.WriteLine("Квартира розташована на 5-ому поверсі");
+                    break;
+
+                case 26:
+                    Console.WriteLine("Квартира розташована на 6-ому поверсі");
+                    break;
+                case 27:
+                    Console.WriteLine("Квартира розташована на 6-ому поверсі");
+                    break;
+                case 28:
+                    Console.WriteLine("Квартира розташована на 6-ому поверсі");
+                    break;
+                case 29:
+                    Console.WriteLine("Квартира розташована на 6-ому поверсі");
+                    break;
+                case 30:
+                    Console.WriteLine("Квартира розташована на 6-ому поверсі");
+                    break;
+
+                case 31:
+                    Console.WriteLine("Квартира розташована на 7-ому поверсі");
+                    break;
+                case 32:
+                    Console.WriteLine("Квартира розташована на 7-ому поверсі");
+                    break;
+                case 33:
+                    Console.WriteLine("Квартира розташована на 7-ому поверсі");
+                    break;
+                case 34:
+                    Console.WriteLine("Квартира розташована на 7-ому поверсі");
+                    break;
+                case 35:
+                    Console.WriteLine("Квартира розташована на 7-ому поверсі");
+                    break;
+
+                case 36:
+                    Console.WriteLine("Квартира розташована на 8-ому поверсі");
+                    break;
+                case 37:
+                    Console.WriteLine("Квартира розташована на 8-ому поверсі");
+                    break;
+                case 38:
+                    Console.WriteLine("Квартира розташована на 8-ому поверсі");
+                    break;
+                case 39:
+                    Console.WriteLine("Квартира розташована на 8-ому поверсі");
+                    break;
+                case 40:
+                    Console.WriteLine("Квартира розташована на 8-ому поверсі");
+                    break;
+
+                case 41:
+                    Console.WriteLine("Квартира розташована на 9-ому поверсі");
+                    break;
+                case 42:
+                    Console.WriteLine("Квартира розташована на 9-ому поверсі");
+                    break;
+                case 43:
+                    Console.WriteLine("Квартира розташована на 9-ому поверсі");
+                    break;
+                case 44:
+                    Console.WriteLine("Квартира розташована на 9-ому поверсі");
+                    break;
+                case 45:
+                    Console.WriteLine("Квартира розташована на 9-ому поверсі");
+                    break;
+            }
+        }
+    }
+}
